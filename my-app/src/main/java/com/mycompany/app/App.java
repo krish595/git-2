abbf307aeed445f614835fe4d955093b9d274687
krish595 <39649585+krish595@users.noreply.github.com>
2019-01-14 16:46:27 +0530

    Update App.java
    commit 2

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hi Hello World!" );
+        System.out.println( "hi krishna Hello World!" );
     }
 }
